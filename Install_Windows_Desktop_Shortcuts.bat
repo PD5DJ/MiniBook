c5a052a7912cf603db28ca15c9bfc04a390776e8
PD5DJ <55426642+PD5DJ@users.noreply.github.com>
2025-06-03 08:10:53 +0200

    cosmetics

--- Install_Windows_Desktop_Shortcuts.bat
+++ Install_Windows_Desktop_Shortcuts.bat
@@ -1,3 +1,3 @@
 @echo off
-CreatePyShortcut.exe MiniBook.py Minibook MiniBook.ico
-CreatePyShortcut.exe HamlibServer.py HamlibServer HamlibServer.ico
+CreatePyShortcut.exe "MiniBook.py" "Minibook" "MiniBook.ico" --w
+CreatePyShortcut.exe "HamlibServer.py" "HamlibServer" "HamlibServer.ico" --w
